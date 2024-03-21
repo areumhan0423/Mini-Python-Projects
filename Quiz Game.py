73382e176b68a6e50ba5861da9281e6d148458db
areumhan0423 <158506119+areumhan0423@users.noreply.github.com>
2024-03-21 13:24:07 +0000

    Update Quiz Game.py
    add comments

--- Quiz Game.py
+++ Quiz Game.py
@@ -1,12 +1,17 @@
 print("Welcome to my computer quiz!")
 
+# start the quiz game when the user types yes and quit otherwise
 play = input("Do you want to play the game? ")
 if play.lower() != 'yes':
     quit()
 
 print("Okay! Let's play :) ")
 
+# initialise score variable starting at 0 
 score = 0
+
+# ask the user questions and stores the answer in the variable answer
+# compare the user's answer by using if statements and add 1 on the variable score 
 answer = input("How many countries are with the UK? ")
 if answer == '4':
     print("Correct!")
@@ -35,5 +40,6 @@ if answer.lower() == 'united states of america':
 else:
     print("Incorrect!")
 
+# print the user's final score by concatenating the number of questions they got correct and its percentage
 print("You got " + str(score) + " questions correct!")
 print("That is " + str((score/4)*100) + "%.")
